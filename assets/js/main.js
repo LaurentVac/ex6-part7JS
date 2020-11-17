6740c2ff2f96abb82ff4db10643bad23ff35aaa4
laurentvac <laurentvac80@gmail.com>
2020-11-17 11:22:18 +0100

    fini

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// (name) => {
+//     `Bonjour, ${name} ! Comment vas-tu ?`;
+// }  
+
+function hello(name){
+    // ne pas oublier return 
+   return `Bonjour, ${name} ! Comment vas-tu ?`;
+}
+ alert(hello('laurent'));
